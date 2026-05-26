b057e429052b78f83fbf0a42ca287e1f7a16a889
Umaid Malik <umaid54321@hotmail.com>
2026-05-26 17:47:49 -0400

    wip

--- incident-management-frontend/vite.config.ts
+++ incident-management-frontend/vite.config.ts
@@ -1,9 +1,8 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -11,4 +10,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  test: {
+    environment: "happy-dom",
+    globals: true,
+    setupFiles: "./src/test/setup.ts",
+  },
+});
